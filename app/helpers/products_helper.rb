82257a31a9963b50d1681c1cee5191f3372552c0
rickrizzle <rickh39@gmail.com>
2019-02-01 13:09:41 -0500

    Add Basic Stock Features

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,12 @@ module ProductsHelper
     def print_price(price)
         number_to_currency price
     end
+
+    def print_stock(stock)
+        if stock <= 0
+            content_tag(:span, "Out of Stock", class: "out_stock")
+        else
+            content_tag(:span, "#{stock} In Stock ", class: "in_stock")
+        end
+    end
 end
