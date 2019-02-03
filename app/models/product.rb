bc52769a42bd01d3728f8c48cd2da03594c0a371
rickrizzle <rickh39@gmail.com>
2019-02-03 15:43:24 -0500

    Add Stock editing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
-    validates_numericality_of :price
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validates :stock, numericality: { 
+        only_integer: true, 
+        greater_than_or_equal_to: 0 }
+
+
 end
